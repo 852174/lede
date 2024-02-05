627d7d7348a65e466a6b15ee9f99b6d5ddeee714
852174 <75799388+852174@users.noreply.github.com>
2024-02-05 22:14:11 +0800

    diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -24,3 +24,4 @@ sed -i 's/${g}.*/${a}${b}${c}${d}${e}${f}${hydrid}/g' package/lean/autocore/file
 date_version=$(date +"%y.%m.%d")
 orig_version=$(cat "package/lean/default-settings/files/zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
 sed -i "s/${orig_version}/R${date_version} by 666/g" package/lean/default-settings/files/zzz-default-settings
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
